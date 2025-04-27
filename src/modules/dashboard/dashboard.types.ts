d76ca52735b1857ee142880ac9208efb79106ef4
Mohitraj27 <Mraj716980@>
2025-04-27 19:34:00 +0530

    Unused Import

--- src/modules/dashboard/dashboard.types.ts
+++ src/modules/dashboard/dashboard.types.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document } from 'mongoose';
+import  { Document } from 'mongoose';
 export interface dashboard extends Document {
     userId: string;
     totalJobPosting: number;
