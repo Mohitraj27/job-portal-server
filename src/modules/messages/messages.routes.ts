945238dd9c07bb0ee072100bb0044699e541432a
Mohitraj27 <raj716980@gmail.com>
2025-05-04 13:48:26 +0530

    Message Count for JobSeeker Dashboard

--- src/modules/messages/messages.routes.ts
+++ src/modules/messages/messages.routes.ts
@@ -1,9 +1,10 @@
 
 import { Router } from 'express';
 import { MessagesController } from './messages.controller';
-import { validategetMessagesByRoomId,validatechatHistorySchema } from './messages.validation';
+import { validategetMessagesByRoomId,validatechatHistorySchema,validateMessageCountforJobSeeker } from './messages.validation';
 const messagesRouter = Router();
 messagesRouter.route('/get-messages-by-roomid').get(validategetMessagesByRoomId, MessagesController.getMessagesByRoomId);
 messagesRouter.route('/history-messages').get(validatechatHistorySchema,MessagesController.getChatHistory);
+messagesRouter.route('/getMessages-count').get(validateMessageCountforJobSeeker,MessagesController.getMessageCountforJobSeeker);
 
 export default messagesRouter;
